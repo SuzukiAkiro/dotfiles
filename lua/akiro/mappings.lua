5b6b9fa73b52ffaca558015c5516f2433fa3cb11
Vladislav Vasilev <akiro_jpg@vk.com>
2023-10-25 23:26:34 +0500

    added toggleterm, vim-surround, autopairs and cursorline

--- lua/akiro/mappings.lua
+++ lua/akiro/mappings.lua
@@ -2,3 +2,4 @@ vim.g.mapleader = " "
 vim.keymap.set("i", "jk", "<esc>")
 vim.keymap.set("n", "<leader>n",  ":NvimTreeToggle<CR>")
 vim.keymap.set("n", "<leader>g", ":LazyGit<CR>")
+vim.keymap.set("n", "<leader>t", ":ToggleTerm<CR>")
