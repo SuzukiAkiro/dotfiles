7859610b038729bbd1daaa4b3dde7d3c898c0ac8
Vladislav Vasilev <akiro_jpg@vk.com>
2023-10-27 23:37:33 +0300

    updated colorscheme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,7 +1,8 @@
 function ColorMe(color)
 	color = color or "gruvbox"
 	vim.cmd.colorscheme(color)
-	
+    vim.cmd("hi CursorLine cterm=underline term=underline guibg=#151515")
+    vim.cmd("hi colorcolumn cterm=underline term=underline guibg=#151515")
 	vim.api. nvim_set_hl(0, "Normal", {bg = "none" })
 	vim.api.nvim_set_hl(0,"NormalFloat", { bg = "none" })
 end
--- init.lua
+++ init.lua
@@ -24,13 +24,10 @@ vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
-
 vim.opt.updatetime = 50
-
-vim.opt.colorcolumn = "88"
 vim.opt.cursorline = true
+vim.opt.colorcolumn = "88"
 vim.g.netrw_browse_split = 3
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 vim.g.netrw_liststyle = 3
--- vim.g.netrw_browse_split = 3
